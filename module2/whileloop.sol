ec719edf4935b5309ac8f8fd6dd91b1d4108e9e3
Asila4ahmed <114068233+Asila4ahmed@users.noreply.github.com>
2023-03-03 23:22:05 +0100

    Create whileloop.sol

---
+++ module2/whileloop.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+
+contract tech4dev{
+    function whileloop() public pure returns(uint){
+        uint i=0; //in here we only initialise i to 0
+
+        while(i<10){
+            i++; //i.e i autoincrementing
+
+        }
+
+    }
+
+}
